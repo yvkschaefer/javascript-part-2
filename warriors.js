4f5a9ff1c7b67397705cd66f558e3032cb517e21
yvkschaefer <yvkschaefer@gmail.com>
2016-07-06 22:32:27 +0000

    question 1 on objects

--- warriors.js
+++ warriors.js
@@ -1,9 +1,7 @@
 /* --------------------------------OBJECTS-------------------------------- */
 
 
-/** Q1. Create a file called `warriors.js`, then, do the following. Commit and push as often as 
-    you like so that we can monitor the progress of your code in the pull request.
-  * Create a constructor function called `Warrior` that can create new warriors. This function 
+/** Q1. Create a constructor function called `Warrior` that can create new warriors. This function 
     will take parameters `name`, and `gender`. `name` can be any string, `gender` can be `M` 
     or `F`. It should create a warrior that has these properties:
     * `name`: the value that was passed to the constructor
@@ -17,3 +15,50 @@
         opponent, this method should output to the console which player won the fight based on 
         their power. Be as creative as you like with the text of this method :)
   * Finally, create a bunch of warriors and make them fight together.*/
+  
+  var warriorPrototype = {
+    name: null,
+    gender: null,
+    level: 1,
+    weapon: 'wooden sword',
+    power: null,
+    fight: function(){
+        var pronoun = null;
+        if(this.gender === 'F'){
+            pronoun = 'her';
+        }
+        else {
+            pronoun = 'his';
+        }
+            console.log(this.name + ' rushes to the arena with ' + pronoun + ' ' + this.weapon + '!');
+        },
+    faceoff: function(opponent){
+        if(this.power > opponent.power){
+        console.log('Wow, an epic battle! ' + this.name + ' pulls out the win!');
+    }
+        else {
+            console.log('Another gnarly fight between these two! This time ' + opponent.name + ' wins!')
+        }
+  }
+  }
+  
+  
+  
+  function Warrior (name, gender){
+      var warrior = Object.create(warriorPrototype)
+      if (typeof name === 'string'){
+          warrior.name = name;
+      }
+      if (gender === 'F' || gender === 'M'){
+          warrior.gender = gender;
+      }
+      warrior.power = Math.random() * 100;
+      return warrior;
+  }
+  
+var aladdin = Warrior('Aladdin', 'M');
+var jasmine = Warrior('Jasmine', 'F');
+
+aladdin.fight();
+jasmine.fight();
+aladdin.faceoff(jasmine);
