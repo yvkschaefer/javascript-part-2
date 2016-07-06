f0caca348821877f7fd2f051cc6d747036e67317
yvkschaefer <yvkschaefer@gmail.com>
2016-07-06 18:27:31 +0000

    question 1

--- functions.js
+++ functions.js
@@ -2,3 +2,20 @@
 a value that sort can use to determine which is the longest string. Finally, create an array of 
 strings and try to get it sorted using your new function.*/
 
+var lakes = ['Huron', 'Erie', 'Ontario', 'Michigan', 'Superior'];
+var birds = ['big', 'ostrich'];
+
+function sortString(string1, string2){
+    if(string1.length > string2.length){
+        return -1        
+    }
+    if(string1.length < string2.length){
+        return 1
+    }
+    else {
+        return 0
+    }
+}
+
+console.log(lakes.sort(sortString));
+console.log(birds.sort(sortString));
